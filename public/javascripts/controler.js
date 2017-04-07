f8ffdda988e57bdf0437d9a7a8db85a8f84c5d05
Stephani McGrath <stephanimcgrath@Stephanis-MacBook-Pro-2.local>
2017-04-07 11:41:09 -0700

    click handeler and edit books synched

--- public/javascripts/controler.js
+++ public/javascripts/controler.js
@@ -1,33 +1,11 @@
 'use strict'
 
-
-
-//grab the node for the delete button
-
-// const deleteButton = document.getElementById('delete')
-// // console.log(deleteButton)
-// deleteButton.addEventListener('click', function() {
-//   // console.log('button clicked')
-//   const bookID = document.getElementById('book-id').value
-//   // console.log('bookID', bookID)
-//   fetch(`/api/books/${bookID}/delete`,
-//         {method: 'POST',
-//          mode: 'cors'})
-//          .then(response => {
-//            window.location = '/'
-//          })
-// })
-
 const thisBook = document.getElementsByClassName('book')
-for(var i =0; i< thisBook.length; i++) {
-thisBook[i].addEventListener('click', () => {
-    const bookID = document.getElementById('book-id').value
-        window.location = `/details/${bookID}`
-      })
-  }
 
+for(let i =0; i < thisBook.length; i++) {
+  let bookId = thisBook[i].lastChild.lastChild.value
 
-
-
-//attach event handler click handler.
-//fire off function when button clicked, to send book id to database
+  thisBook[i].addEventListener('click', () => {
+    window.location = `http://localhost:3000/details/${bookId}`
+  })
+}
