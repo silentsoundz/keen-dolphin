465d2e630367b2547fcf84d65dac8d8d816ba68f
yaseenagag <yaseen.uc@gmail.com>
2017-05-22 14:02:16 -0700

    final updates

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ server.post('/api/test/reset-db', (request, response, next) =>{
 })
 
 server.post('/api/books', (request, response, next) => {
-  if ( request.body.title !== undefined ) {
+  if ( request.body.hasOwnProperty('title') ) {
     request.body.genres = request.body.genres.split(',')
     db.createWholeBook(request.body).then(book => {
       response.redirect('/')
